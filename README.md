6f7996aef335aacab905bca1a82b94fcfe63aa08
Chengsu Chen <cchengsu@gmail.com>
2015-11-09 23:52:24 -0800

    adding all files

---
+++ README.md
@@ -0,0 +1,19 @@
+What is the URL of this site, as hosted on your student webserver (i.e.,
+
+students.washington.edu/cchen94/info343/dawg-coffee/index.html 
+
+2. Did you receive any help from others (classmates, etc)? If so, please list who.
+
+Gabriel Ho
+
+3. Did you complete any advanced extensions to this challenge? If so, what?
+
+No.
+
+4. Approximately how many hours did it take you to complete this challenge?
+
+6
+
+5. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+
+Learning Spotify's API in addition to angular JS was a bit overwhelming.
---
+++ main.js
@@ -0,0 +1,47 @@
+var data;
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var relatedUrl = 'https://api.spotify.com/v1/artists/'
+var trackUrl = 'https://api.spotify.com/v1/tracks/'
+var artistUrl = 'https://api.spotify.com/v1/artists/'
+var myApp = angular.module('myApp', [])
+
+var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
+  $scope.audioObject = {}
+  $scope.getSongs = function() {
+    $http.get(baseUrl + $scope.track).success(function(response){
+      data = $scope.tracks = response.tracks.items
+    })
+  }
+
+//this function is supposed to display related artists of the track
+  $scope.getRelated = function(track){
+    var relatedArtist = [];
+    $http.get(trackUrl + $scope.track).success(function(response){
+      var artists = response.artists[0];
+      $http.get(relatedUrl + $scope.artist + '/related-artists').success(function(response){
+        relatedArtist.push(artists)
+    })
+  })
+}
+
+  $scope.play = function(song) {
+    if($scope.currentSong == song) {
+      $scope.audioObject.pause()
+      $scope.currentSong = false
+      return
+    }
+    else {
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      $scope.audioObject = new Audio(song);
+      $scope.audioObject.play()  
+      $scope.currentSong = song
+    }
+  }
+
+
+})
+
+// Add tool tips to anything with a title property
+$('body').tooltip({
+    selector: '[title]'
+});
